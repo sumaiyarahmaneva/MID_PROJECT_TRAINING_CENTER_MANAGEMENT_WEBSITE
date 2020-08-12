f0fbb560fef7866c54166b1dd91260bb5030ea31
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-08-13 01:08:07 +0600

    notice_demo_php

---
+++ notice.php
@@ -0,0 +1,94 @@
+<?php
+	session_start();
+	if(!empty($_SESSION))
+	{
+		if(empty($_SESSION['status']) || $_SESSION['usertype']!="Student")
+		{
+			header('location:logout.php');
+		}
+	}
+	else
+	{
+		if(empty($_COOKIE['status']) || $_SESSION['usertype']!="Student")
+		{
+			header('location:logout.php');
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Notice</title>
+</head>
+<body>
+	<fieldset>
+		<p>NSS Traning Center</p>
+		<?php
+			if(!empty($_SESSION))
+			{
+				echo "<p align='right'>Logged in as <a href='owninformation.php'>".$_SESSION['name']."</a>|<a href='logout.php'>Logout</a></p>";
+			}
+			else
+				echo "<p align='right'>Logged in as <a href='owninformation.php'>".$_COOKIE['name']."</a>|<a href='logout.php'>Logout</a></p>";
+		?>
+	</fieldset>
+	<fieldset>
+		<table width="100%" border="1">
+			<tr>
+				<td rowspan="6" width="30%">
+					Student Account
+					<hr/>
+					<ul>
+						<li><a href="student.php">Student Home</a></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+				</td>
+				<td rowspan="6">
+					<fieldset>
+					    <legend><b>Notice</b></legend>
+						<form>
+							<br/>
+							<table>
+								<tr>
+									<td width="20%">Notice</td>
+									<td></td>
+									<td>
+									    Notice Details
+									</td>
+								</tr>
+								<tr>
+									<td width="20%">Official Notice</td>
+									<td>:</td>
+									<td>
+										<a href="">Show</a>
+									</td>
+								</tr>
+								<tr>
+									<td width="20%">C# Notice</td>
+									<td>:</td>
+									<td>
+										<a href="">Show</a>
+									</td>
+								</tr>
+								<tr>
+									<td width="20%">Web Tech Notice</td>
+									<td>:</td>
+									<td>
+										<a href="">Show</a>
+									</td>
+								</tr>
+								<tr>
+									<td width="20%">Java Notice</td>
+									<td>:</td>
+									<td>
+										<a href="">Show</a>
+									</td>
+								</tr>
+							</table>		
+						</form>
+					</fieldset> 
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+</body>
