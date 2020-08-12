0bee089fed3ff8c0a3a644ddc70ba35d77415ede
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-08-13 00:55:08 +0600

    applyjob_demo_php

---
+++ applyforjob.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+	if(!empty($_SESSION))
+	{
+		if(empty($_SESSION['status']) || $_SESSION['usertype']!="Student")
+		{
+			header('location:logout.php');
+		}
+	}
+	else
+	{
+		if(empty($_COOKIE['status']) || $_SESSION['usertype']!="Student")
+		{
+			header('location:logout.php');
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apply For Job</title>
+</head>
+<body>
+	<fieldset>
+		<p>NSS Traning Center</p>
+		<?php
+			if(!empty($_SESSION))
+			{
+				echo "<p align='right'>Logged in as <a href='owninformation.php'>".$_SESSION['name']."</a>|<a href='logout.php'>Logout</a></p>";
+			}
+			else
+				echo "<p align='right'>Logged in as <a href='owninformation.php'>".$_COOKIE['name']."</a>|<a href='logout.php'>Logout</a></p>";
+		?>
+	</fieldset>
+	<fieldset>
+		<table width="100%" border="1">
+			<tr>
+				<td rowspan="6" width="30%">
+					Student Account
+					<hr/>
+					<ul>
+						<li><a href="student.php">Student Home</a></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+				</td>
+				<td rowspan="6">
+					<fieldset>
+					    <legend><b>Job Application</b></legend>
+						<form>
+							<br/>
+							<table>
+								<tr>
+									<td width="20%">Apply For Job</td>
+									<td>:</td>
+									<td>
+										<input type="file" name="jobapply">
+									</td>
+								</tr>
+								<tr>
+									<td width="20%" colspan="3">
+										<input type="submit" name="submit" value="submit">
+									</td>
+								</tr>
+							</table>		
+						</form>
+					</fieldset> 
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+</body>
